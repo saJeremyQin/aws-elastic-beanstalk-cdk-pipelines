e9e9c7c917770b5f34f7c3602f078e3d95c4e231
saJeremyQin <343220868@qq.com>
2023-09-25 09:45:33 +0930

    Add Pre-Prod stage

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,6 +1,8 @@
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CdkEBStage } from './eb-stage';
+
 
 /**
  * The stack that defines the application pipeline
@@ -10,24 +12,31 @@ export class CdkPipelineStack extends Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      // The pipeline name
-      pipelineName: 'MyServicePipeline',
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('saJeremyQin/aws-elastic-beanstalk-cdk-pipelines', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-        //  installCommands: ['npm i -g npm@latest'],
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
+        // The pipeline name
+        pipelineName: 'MyServicePipeline',
+
+        // How it will be built and synthesized
+        synth: new ShellStep('Synth', {
+            // Where the source can be found
+            input: CodePipelineSource.gitHub('saJeremyQin/aws-elastic-beanstalk-cdk-pipelines', 'main'),
+            
+            // Install dependencies, build and run cdk synth
+            //  installCommands: ['npm i -g npm@latest'],
+            commands: [
+                'npm ci',
+                'npm run build',
+                'npx cdk synth'
+            ],
+        }),
     });
 
     // This is where we add the application stages
+    // For environment with custom AutoScaling group configuration
+    const deploy = new CdkEBStage(this, 'Pre-Prod', { 
+        minSize : "1",
+        maxSize : "2"
+    });
+    const deployStage = pipeline.addStage(deploy); 
   }
 }
